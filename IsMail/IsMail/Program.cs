688d020b68fbe7ff6199e7f1879cb2376cf8871c
Lukas Papariga <lukaspapariga@gmail.com>
2022-10-11 21:23:07 +0200

    Getter, setter -> basics, mailValidator
    getter, setter first program
    mail validator using regex (not built-in IsMail method)

---
+++ IsMail/IsMail/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IsMail;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string userMail = "papariga.lu.2020@ssps.cz";
+
+        Console.WriteLine(IsMail(userMail));
+
+        string[] userMails = { "jan.novak@seznam.cz", "jannovak@mail.cz", "jan_novak111@centrum.cz", "@mail.cz", "ahoj.cz" };
+
+        foreach (string mail in userMails)
+        {
+            Console.WriteLine(IsMail(mail));
+        }
+
+        // IsMail() -> no need for regex
+
+        // Console.WriteLine("Hack");
+        // 
+        // try
+        // {
+        //     Console.WriteLine(IsMail(userMail));
+        // }
+        // catch
+        // {
+        //     Console.WriteLine("Not good");
+        // }
+
+    
+
+        Console.ReadKey();
+    }
+
+    public static bool IsMail(string userMail)
+    {
+        Regex validatro = new Regex(@"^[\[\w\._-]+@[\w]+\.\w{1,18}$");
+
+        return validatro.IsMatch(userMail);
+    }
+
+
+}
---
+++ getSetTest/getSetTest/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace getSetTest;
+
+class Program
+{
+    public static void Main()
+    {
+        Test bobus = new();
+
+        bobus.MaxLoad = 200;
+
+        Console.WriteLine(bobus.MaxLoad);
+
+        Console.ReadKey();
+    }
+}
+
+class Test
+{
+    private double maxLoad;
+    public double MaxLoad
+    {
+        get { return maxLoad;  }
+        set {
+            Console.WriteLine($"Prirazuji {value}");
+            if (value > 0)
+            {
+                this.maxLoad = value;
+            }
+            else
+            {
+                this.maxLoad = 0;
+            }
+        }
+    }
+}
