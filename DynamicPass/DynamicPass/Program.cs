c1a0140f0be4a560140bea822f42a2ec258eeb8d
Lukas Papariga <lukaspapariga@gmail.com>
2022-11-14 15:01:00 +0100

    Custom readline
    dalsi lepsi algo nez ma honza (je rychlejsi, more readable a navic jsem pomohl honzovi)

---
+++ DynamicPass/DynamicPass/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DynamecPass;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string s = IsStringValid(Validate);
+        Console.ReadKey();
+    }
+
+    public static string IsStringValid(Something f)
+    {
+        string? userInput;
+        while (true)
+        {
+            userInput = Console.ReadLine();
+            if (f(userInput))
+            {
+                Console.WriteLine("Correct");
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Bad string format, try again");
+            }
+        }
+    }
+
+    public static bool Validate(string? userInput)
+    {
+        if (userInput != null && userInput.Length >= 2 && Int32.TryParse(userInput, out _))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public delegate bool Something(string? userInput);
+}
