d58ee28a7740f6a05ad80370e30f49d344f93c96
Lukas Papariga <lukaspapariga@gmail.com>
2022-11-23 08:46:45 +0100

    Events, Pathfinding inicialization

---
+++ Event/Event/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Event;
+
+class Program
+{
+
+
+    public static void Main(string[] args)
+    {
+        AlertList al = new AlertList(new List<int>(), 20);
+        al.Capcity += al_Capacity;
+        al.Overflow += al_Overflow;
+
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+        al.AddItem(100);
+
+
+        Console.ReadKey();
+    }
+
+    private static void al_Overflow(int maxSize)
+    {
+        Console.WriteLine($"max size has been reached (max size: {maxSize})");
+    }
+
+    private static void al_Capacity(double percent)
+    {
+        Console.WriteLine($"{100 - percent}% free space");
+    }
+}
+
+class AlertList
+{
+    public delegate void OverflowHandler(int maxSize);
+    public delegate void CapacityHandler(double sizeFilledPercent);
+
+    public event OverflowHandler Overflow;
+    public event CapacityHandler Capcity;
+
+    private int maxSize;
+    public int MaxSize
+    {
+        get
+        {
+            return maxSize;
+        }
+
+        set
+        {
+            maxSize = value;
+        }
+    }
+
+
+
+    private List<int> list;
+    public List<int> List
+    {
+        get
+        {
+            return list;
+        }
+
+        set
+        {
+            list = value;
+        }
+    }
+
+    public AlertList(List<int> list, int maxSize)
+    {
+        this.List = new List<int>();
+        this.MaxSize = maxSize;
+    }
+
+
+    public void AddItem(int number)
+    {
+        if (this.List.Count < this.MaxSize)
+        {
+            this.List.Add(number);
+            Capcity.Invoke((double)this.List.Count / this.MaxSize * 100);
+        }
+        else
+        {
+            Overflow.Invoke(this.MaxSize);
+        }
+    }
+}
---
+++ PathFinding/PathFinding/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PathFinding;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] maze = {
+            { 0, 0, 0, 0, 0},
+            { 0, 1, 1, 1, 1},
+            { 0, 0, 0, 0, 0},
+            { 0, 1, 1, 0, 0},
+            { 1, 1, 1, 0, 1}
+        };
+        // { 0, 0, 0, Z, 0}
+        // { 0, 1, 1, 1, 1}
+        // { 0, 0, 0, 0, 0}
+        // { 0, 1, 1, K, 0}
+        // { 1, 1, 1, 0, 1}
+
+        int XBegin = 3;
+        int YBegin = 0; // Y is reversed to match indexes of array
+
+        int XEnd = 3;
+        int YEnd = 3; // Y is reversed here too
+
+    }
+
+    public static List<int[]> NextStep(char[,] maze, int XBegin, int YBegin, int XEnd, int YEnd, List<int[]> stepsForRobot)
+    {
+        if (XBegin == XEnd && YBegin == YEnd)
+        {
+            return stepsForRobot;
+        }
+        else
+        {
+            try:
+                if ()
+        }
+    }
+}
