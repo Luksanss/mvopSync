3b3ce616cace1a8991b8d6c4a58409f2ee1023e9
Lukas Papariga <lukaspapariga@gmail.com>
2022-12-12 13:50:22 +0100

    Testing Github in visual studio

--- PathFinding/PathFinding/Program.cs
+++ PathFinding/PathFinding/Program.cs
@@ -16,25 +16,114 @@ class Program
         // { 0, 0, 0, 0, 0}
         // { 0, 1, 1, K, 0}
         // { 1, 1, 1, 0, 1}
+       
+        int[] Begin = { 0, 3 };
 
-        int XBegin = 3;
-        int YBegin = 0; // Y is reversed to match indexes of array
+        int[] End = { 3, 3 }; 
 
-        int XEnd = 3;
-        int YEnd = 3; // Y is reversed here too
+        SortedDictionary<int[], char> dict = new SortedDictionary<int[], char>();
+        dict.Add(Begin, 'Z');
 
+        foreach (KeyValuePair<int[], char> kvp in Step(maze, Begin, End, dict))
+        {
+            Console.WriteLine($"{kvp.Key} on: {kvp.Value}");
+        }
     }
 
-    public static List<int[]> NextStep(char[,] maze, int XBegin, int YBegin, int XEnd, int YEnd, List<int[]> stepsForRobot)
+    public static SortedDictionary<int[], char> Move(SortedDictionary<int[], char> dict, int[,] maze)
     {
-        if (XBegin == XEnd && YBegin == YEnd)
+        char[] moves = { 'l', 'r', 'u', 'd' }; // l = left, r = right, u = up,  d = down
+
+        int[] curPos = dict.Last().Key;
+        char curMove = dict.Last().Value;
+        switch (curMove)
         {
-            return stepsForRobot;
+            case 'Z':
+                try
+                {
+                    if (maze[curPos[0], curPos[1] - 1] != 1)
+                    {
+                        dict.Add(new int[] { curPos[0], curPos[1] - 1}, 'l');
+                        return dict;
+                    }
+                }
+                catch
+                {
+                    goto case 'l';
+                }
+                break;
+            case 'l':
+                try
+                {
+                    if (maze[curPos[0], curPos[1] + 1] != 1)
+                    {
+                        dict.Add(new int[] { curPos[0], curPos[1] + 1 }, 'r');
+                        return dict;
+                    }
+                }
+                catch
+                {
+                    goto case 'r';
+                }
+                break;
+            case 'r':
+                try
+                {
+                    if (maze[curPos[0] - 1, curPos[1]] != 1)
+                    {
+                        dict.Add(new int[] { curPos[0] - 1, curPos[1] }, 'u');
+                        return dict;
+                    }
+                }
+                catch
+                {
+                    goto case 'u';
+                }
+                break;
+
+
+            case 'u':
+                try
+                {
+                    if (maze[curPos[0] + 1, curPos[1]] != 1)
+                    {
+                        dict.Add(new int[] { curPos[0] + 1, curPos[1] }, 'd');
+                        return dict;
+                    }
+                }
+                catch
+                {
+                    goto case 'd';
+                }
+                break;
+            case 'd':
+                try
+                {
+                    if (maze[curPos[0], curPos[1] - 1] != 1)
+                    {
+                        dict.Add(new int[] { curPos[0], curPos[1] - 1 }, 'l');
+                        return dict;
+                    }
+                }
+                catch
+                {
+                    goto case 'u';
+                }
+                break;
+            default:
+                return dict;
         }
-        else
+        return dict;
+    }
+
+    public static SortedDictionary<int[], char> Step(int[,] maze, int[] begin, int[] end, SortedDictionary<int[], char> dict)
+    {
+        // runs limit after completing alg
+        if (dict.Last().Key == end)
         {
-            try:
-                if ()
+            return dict;
         }
+        var nextMove = Move(dict, maze);
+        return Step(maze, begin, end, nextMove);
     }
 }
