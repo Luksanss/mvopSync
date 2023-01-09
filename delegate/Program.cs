a85fee5134d84901b95064cee49d6d37aa5e9ae7
Lukas Papariga <lukaspapariga@gmail.com>
2023-01-09 12:51:25 +0100

    delegate

---
+++ delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace f;
+
+class Program
+{
+
+    public delegate int AddInt(int a);
+    public static int Add5(int a)
+    {
+        Console.Write("Add5 ");
+        return a + 5;
+    }
+    public static int Add10(int a)
+    {
+        Console.Write("Add10 ");
+        return a + 10;
+    }
+    public static void Main(string[] args)
+    {
+        AddInt fi, sumFi = null;
+        fi = Add5;
+        Console.WriteLine(fi(3)); // Add5 8
+        sumFi += fi;
+        fi = Add10;
+        Console.WriteLine(fi(3)); // Add10 13
+        sumFi += fi;
+        Console.WriteLine(sumFi(3)); // Add5 Add10 13
+        Console.ReadKey();
+    }
+}
