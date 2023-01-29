d45ac69b93d63478d57c8b3ff356dfb0ca8861c7
Lukas <papariga.lu.2020@skola.ssps.cz>
2023-01-29 13:02:12 +0100

    upgrading
    Rozšiřte řešení příkladu 6 o:
    odlišení povinných vstupů: příjmení, pracovní pozice, hrubý plat
    validace vstupů - chybné zadání indikujte graficky ihned po opuštění pole, chybové hlášení umístěte poblíž vstupního pole
    korporátní barvy pro formulář a ovládací prvky: paleta
    načítání zadaných dat ze souboru do prvku ListView a možnost jejich modifikace a hromadného zápisu
    dynamicky náhled zápisu záznamu do souboru (ukažte na GUI, jaký string/JSON se bude ukládat při aktuálních datech)
    Pro soubor s osobami, u kterých je evidované jméno, příjmení, datum narození a rodné číslo vytvořte pomocí ListView prohlížeč, který má v levém sloupci jméno a příjmení a v pravém sloupci všechny detaily o osobě.

---
+++ wpfFirstHomework/mvopWpfHomework/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mvopWpfHomework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /*
+        Napište ve WPF vstupní formulář pro třídu Zaměstnanec, která obsahuje jméno, příjmení, rok narození, nejvyšší dokončené vzdělání(výběrem z omezených možností), pracovní pozici a hrubý plat v korunách.
+        Třídu Zaměstnanec vytvořte děděním ze třídy Osoba, která obsahuje jméno, příjmení a rok narození.
+        Formulář otestujte a informace o zaměstnancích ukládejte do souboru.
+     */
+    public partial class MainWindow : Window
+    {
+        Zamestnanec zm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = zm = new Zamestnanec();
+        }
+        private void PrintToLabel(object sender, RoutedEventArgs e)
+        {
+            LabelEmploee.Content = zm.ToString();
+        }
+
+        private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            string Path = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "\\dataEmployes.csv";
+            using (StreamWriter sw = File.AppendText(Path))
+            {
+                sw.WriteLine($"Name: {zm.Name}, Surname: {zm.Surname}, Date of birth {zm.DateOfBirth}, Education: {zm.EdDegree}, Salary: {zm.Salary}, Occupation: {zm.WorkOccupation}");
+            }
+        }
+    }
+
+    class Osoba 
+    {
+        string name;
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        DateTime dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+    }
+
+    class Zamestnanec : Osoba
+    {
+        string edDegree;
+        public string EdDegree
+        { 
+            get { return edDegree; }
+            set { edDegree = value; }
+        }
+
+        string workOccupation;
+        public string WorkOccupation
+        {
+            get { return workOccupation; }
+            set { workOccupation = value; }
+        }
+
+        double salary;
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Surname},\nBorn: {DateOfBirth}, \nDegree: {edDegree} \nWorking as: {workOccupation}, salary: {salary}";
+        }
+
+    }
+}
