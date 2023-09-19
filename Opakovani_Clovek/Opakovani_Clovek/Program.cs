67afed7fd02141132246f36ddea892cda6eff451
Lukas <91434573+Luksanss@users.noreply.github.com>
2023-09-19 15:26:15 +0200

    Opakovani: OOP, Geometrie
    Clovek, Osoba -> OOP + regex
    Teleso -> OOP, abstract + geometrie

---
+++ Opakovani_Clovek/Opakovani_Clovek/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Opakovani_Clovek;
+
+class Program
+{
+    class Clovek
+    {
+        private string jmeno;
+        public string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            set
+            {
+                jmeno = value;
+            }
+        }
+
+        private string prijmeni;
+        public string Prijmeni
+        {
+            get
+            {
+                return prijmeni;
+            }
+            set
+            {
+                prijmeni = value;
+            }
+        }
+
+        // constructor without params
+        public Clovek()
+        {
+            Jmeno = "Nezname";
+            Prijmeni = "Nezname";
+        }
+
+        // constructor with params
+        public Clovek(string name, string surname)
+        {
+            Jmeno = name;
+            Prijmeni = surname;
+        }
+
+        public override string ToString()
+        {
+            return $"Jmeno osoby je: {this.Jmeno}\nPrijimeni jest: {this.Prijmeni}";
+        }
+    }
+
+    class Obcan : Clovek
+    {
+        private string rodneCislo;
+        public string RodneCislo
+        {
+            get
+            {
+                return rodneCislo;
+            }
+
+            set
+            {
+                rodneCislo = value;
+            }
+        }
+
+
+        public Obcan() : base()
+        {
+            RodneCislo = "000000/0000";
+        }
+
+        public Obcan(string name, string surname, string rodneC) : base(name, surname)
+        {
+            // check validity
+            Regex rg = new Regex(@"^\d{6}\/\d{4}$");
+
+            if (!rg.IsMatch(rodneC))
+            {
+                throw new FormatException();
+            }
+
+            RodneCislo = rodneC;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Jmeno}\nSurname: {this.Prijmeni}\nID: {this.RodneCislo}";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        Obcan novyClovek = new Obcan("jmeno", "prijemnei", "444345/3245");
+
+        
+        Console.WriteLine(novyClovek);
+
+        Console.ReadKey();
+    }
+
+}
+
---
+++ Opakovani_Teleso/Opakovani_Teleso/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace Opakovani_Teleso;
+
+class Program
+{
+    abstract class Teleso
+    {
+        public abstract double Povrch();
+        public abstract double Objem();
+
+        public double PomerPovrchObjem()
+        {
+            return Povrch() / Objem();
+        }
+    }
+
+    class Valec : Teleso
+    {
+        private double polomerPodstavy;
+        public double PolomerPodstavy
+        {
+            get
+            {
+                return polomerPodstavy;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception();
+                }
+
+                polomerPodstavy = value;
+
+            }
+        }
+
+        private double vyska;
+        public double Vyska
+        {
+            get
+            {
+                return vyska;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception();
+                }
+                vyska = value;
+            }
+        }
+
+        public override double Povrch()
+        {
+            return 2 * (Math.PI * polomerPodstavy * polomerPodstavy) + (2 * Math.PI * polomerPodstavy * Vyska);
+        }
+
+        public override double Objem()
+        {
+            return Math.PI * (polomerPodstavy * polomerPodstavy) * Vyska;
+        }
+
+        public static string KouleKuValci(double polomerKoule)
+        {
+            Koule novaKoule = new Koule { Polomer = polomerKoule };
+            Valec novyValec = new Valec { vyska = 4d / 3d * polomerKoule, polomerPodstavy = polomerKoule};
+            return $"Pomer povrchu ku objemu koule: {novaKoule.PomerPovrchObjem()}\nPomer povrchu ku objemu valec: {novyValec.PomerPovrchObjem()}";
+        }
+    }
+
+    class Koule : Teleso
+    {
+        private double polomer;
+        public double Polomer
+        {
+            get
+            {
+                return polomer;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception();
+                }
+
+                polomer = value;
+
+            }
+        }
+
+        public override double Povrch()
+        {
+            return 4 * Math.PI * Polomer * Polomer;
+        }
+
+        public override double Objem()
+        {
+            return 4d/3d * Math.PI * Math.Pow(Polomer, 3);
+        }
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Valec mujV = new Valec { PolomerPodstavy = 5, Vyska = 3 };
+
+        Koule mojeK = new Koule { Polomer = 43 };
+
+        Console.WriteLine(Valec.KouleKuValci(3));
+
+        Console.ReadKey();
+    }
+}
