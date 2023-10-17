ab78903a5e74cdb7c1b9f84bdf0743cf7cbe35e2
Papariga Lukjans <papariga.lu.2020@skola.ssps.cz>
2023-10-17 14:29:45 +0200

    async faster?

---
+++ AsyncFillPrimeArray/AsyncFillPrimeArray/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncFillPrimeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("10-14 mil range: ");
+            FillArayWithPrimes(1000000, 1400000);
+            Console.WriteLine("faster?");
+            RunFour();
+
+            Console.ReadKey();
+        }
+
+        public async static void RunFour()
+        {
+            await Task.Run(() =>
+            {
+                return FillArayWithPrimes(1000000, 1100000);
+            });
+            await Task.Run(() =>
+            {
+                return FillArayWithPrimes(1100000, 1200000);
+            });
+            await Task.Run(() =>
+            {
+                return FillArayWithPrimes(1200000, 1300000);
+            });
+            await Task.Run(() =>
+            {
+                return FillArayWithPrimes(1300000, 1400000);
+            });
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) { return false; }
+            if (number == 2) { return true; }
+            if (number % 2 == 0) { return false; }
+
+            var boundary = Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        public static List<int> FillArayWithPrimes(int lowerRange, int upperRange)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            List<int> primeArr = new List<int>();
+
+            for (int i = lowerRange; i < upperRange; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primeArr.Add(i);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            return primeArr;
+        }
+    }
+}
