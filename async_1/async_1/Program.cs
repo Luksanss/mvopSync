0d6796e7f89b7cfd73215d5d6c76fd980ddbe334
Lukas <91434573+Luksanss@users.noreply.github.com>
2024-01-19 23:20:11 +0100

    async

---
+++ async_1/async_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace async_1;
+
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        CountLeftAsync();
+        CountRightAsync();
+        Console.ReadKey();
+    }
+
+    static async Task CountLeftAsync()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.SetCursorPosition(0, 2);
+            Console.Write($"{i} na threadu {Thread.CurrentThread.Name}{Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(100);
+        }
+        
+    }
+
+    static async Task CountRightAsync()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.SetCursorPosition(60, 23);
+            Console.Write($"{i} na threadu {Thread.CurrentThread.Name}{Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(100);
+        }
+
+    }
+}
