ce8a47bb4e55c69e4d1ac34b490c2a4fa9a76c72
Lukas <91434573+Luksanss@users.noreply.github.com>
2024-02-20 13:37:18 +0100

    Steganography innit

---
+++ Steganography/Steganography/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Steganography;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string pathToImage = args[0];
+
+        Bitmap newImage = null;
+        using (var image = new Bitmap(pathToImage))
+        {
+            newImage = new Bitmap(image);
+
+        }
+
+
+    }
+
+}
