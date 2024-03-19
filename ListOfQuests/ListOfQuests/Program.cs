056b5883c15d126441b810726d85703b33d0bbba
Lukas <91434573+Luksanss@users.noreply.github.com>
2024-03-19 14:11:25 +0100

    sumaceFaktorialu, basic ulohy

---
+++ ListOfQuests/ListOfQuests/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ListOfQuests;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // a)
+        Console.WriteLine(IsEven(2));
+        Console.WriteLine(IsEven(3));
+
+        // b)
+        Console.WriteLine(IsConvertable("4"));
+        Console.WriteLine(IsConvertable("4fd"));
+
+        // c)
+        string hey = "hey";
+        string hello = "hello";
+
+        Console.WriteLine(hey);
+        Console.WriteLine(hello);
+
+        Swap(ref hey, ref hello);
+
+        Console.WriteLine(hey);
+        Console.WriteLine(hello);
+
+        // d)
+        string[] arr = { "f", "asdf", "asdf", "Luasd" };
+
+        foreach (var elem in GetLengthOfArray(arr))
+        {
+            Console.Write(elem);
+        }
+
+        Console.ReadKey();
+    }
+
+    public static bool IsEven(int i)
+    {
+        return i % 2 == 0;
+    }
+
+    public static int IsConvertable(string sentence)
+    {
+        int.TryParse(sentence, out int res);
+
+        if (res == 0) return int.MinValue;
+        return res;
+    }
+
+    public static void Swap(ref string s, ref string t)
+    {
+        (s, t) = (t, s);
+    }
+
+    public static int[] GetLengthOfArray(string[] arr)
+    {
+        int[] arrLen = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arrLen[i] = arr[i].Length;
+        }
+
+        return arrLen;
+    }
+}
---
+++ sumaceFaktorial/sumaceFaktorial/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace sumaceFaktorial;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(6));
+
+        Console.WriteLine(SumFact(2, 4));
+
+        Console.ReadKey();
+    }
+
+    public static double SumFact(int x, int n)
+    {
+        double sum = 1;
+
+        bool substract = false;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (!substract)
+            {
+                sum += Math.Pow(x, i) / Factorial(i);
+                substract = true;
+            }
+            else
+            {
+                sum -= Math.Pow(x, i) / Factorial(i);
+                substract = false;
+            }
+            
+        }
+
+        return sum;
+    }
+
+    public static int Factorial(int n)
+    {
+        int result = 1;
+
+        int tempN = n;
+        while (tempN > 0)
+        {
+            result *= tempN;
+            tempN--;
+        }
+
+        return result;
+    }
+}
