83d8a37557e83035c004e4af0569b9e69720fb08
Lukas <91434573+Luksanss@users.noreply.github.com>
2024-04-09 14:59:52 +0200

    SellingPlatform

---
+++ SellingPlatform/SellingPlatform/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SellingPlatform;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.ReadKey();
+    }
+}
+
+public struct Address
+{
+    string street;
+    string number;
+    string town;
+    public override string ToString()
+    {
+        return $"{street} {number} {town}";
+    }
+}
+
+public interface ISellable
+{
+    decimal Price { get; set; }
+
+    DateTime AvailableSince { get; set; }
+
+    Address GetLocation();
+}
+
+public class Picture : ISellable
+{
+    public string Author { get; set; }
+
+    public double Length { get; set; }
+    
+    public double Width { get; set; }
+
+    // ISellable implementation
+
+    public decimal Price { get; set; }
+    public DateTime AvailableSince { get; set; }
+
+    public Address Location { get; set; }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
