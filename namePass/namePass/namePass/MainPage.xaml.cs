b51937c32dbbb14112168a3d69c101b7020e2c7e
Lukas <91434573+Luksanss@users.noreply.github.com>
2024-06-10 20:42:07 +0200

    latest

---
+++ namePass/namePass/namePass/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace namePass
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+
+        void ButtonSend_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var a = loginName.Text;
+        }
+
+    }
+}
+
---
+++ namePass/namePass/namePass/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace namePass
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        // text and visibility of status label
+        private string statusLabelText;
+
+        public string StatusLabelText { get => statusLabelText; set => SetProperty(ref statusLabelText, value); }
+
+        private bool loginStatusVisible;
+
+        public bool LoginStatusVisible { get => loginStatusVisible; set => SetProperty(ref loginStatusVisible, value); }
+
+    }
+}
+
+
